baf9880ee1e0813aee5c19b4cad8c8dac7f210e5
arandomgal <michelle.andrews31313@gmail.com>
2016-07-19 14:31:00 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', '[' + socket.client.id + ']: ' + msg);
+  });
+  socket.on('disconnect', function() {
+    io.emit('chat message', '---' + socket.client.id + ' just left the room!!!');
+  });
+});
+
+io.on('connect', function(socket){
+  io.emit('chat message', '---' + socket.client.id + ' just joined the room!!!');
+});
+
+http.listen(process.env.PORT || '3333', function(){
+  console.log('listening on port:' + process.env.PORT || '3333');
+});
